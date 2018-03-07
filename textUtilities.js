d140d42e535b2f3297a0606ed86dd3c7e59ad16a
Birna Birna <birnabirna@Birnas-MBP-2.tskoli.is>
2018-03-07 09:52:54 +0000

    AssertionError: expected undefined to be a string

--- textUtilities.js
+++ textUtilities.js
@@ -1,5 +1,9 @@
 var expect = require('chai').expect;
 
-expect(true).to.be.true
+expect(true).to.be.true;
+
+function titleCase() {
+	
+}  
 
 expect(titleCase('the great mouse detective')).to.be.a('string');
